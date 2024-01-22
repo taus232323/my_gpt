13fecd3d15229e692d1bf011437b315c409b1871
taus232323 <tayc13@list.ru>
2024-01-22 17:21:47 +0300

    tried to do context

--- probe.py
+++
@@ -1,3 +0,0 @@
-# await update.message.reply_text("Загружаю ответ...")
-# await context.bot.delete_message(chat_id=update.message.chat_id, message_id=update.message.message_id)
-        
--- response.py
+++ response.py
@@ -1,10 +1,10 @@
 import g4f
 
-def get_response(question):
+def get_gpt4_response(question):
     response = g4f.ChatCompletion.create(
     model=g4f.models.gpt_4,
     messages=[{"role": "user", "content": question}],
 )
     return response
 
-# print(get_response())
+# print(get_response("Можно ли вызывать синхронную фунцию в асихронном коде python?"))
